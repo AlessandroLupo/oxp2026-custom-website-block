robinlej/oxp2025-interactions

Step 3: add interaction (dynamic content)

interactions_demo/controllers/controllers.py
@@ -1,0 +1,38 @@
+from odoo import http
+
+
+class InteractionsDemo(http.Controller):
+    @http.route('/interactions_demo/get_weather', auth='public', type='jsonrpc')
+    def get_weather(self):
+        # Data based on API call to Open-Meteo.com
+        return {
+            "daily": {
+                "time": [
+                    "2025-09-18",
+                    "2025-09-19",
+                    "2025-09-20",
+                    "2025-09-21",
+                    "2025-09-22",
+                    "2025-09-23",
+                    "2025-09-24",
+                ],
+                "temperature_2m_max": [
+                    11.2,
+                    11.3,
+                    11.6,
+                    11.9,
+                    10.0,
+                    11.0,
+                    9.6
+                ],
+                "weathercode": [
+                    0,
+                    80,
+                    18,
+                    3,
+                    61,
+                    3,
+                    61
+                ]
+            }
+        }

interactions_demo/static/src/interactions/weather.js
@@ -1,0 +1,73 @@
+import { formatDate, parseDate } from "@web/core/l10n/dates"
+import { rpc } from "@web/core/network/rpc";
+import { registry } from "@web/core/registry";
+import { Interaction } from "@web/public/interaction";
+
+export class Weather extends Interaction {
+    static selector = ".s_weather";
+
+    dynamicContent = {
+        ".weather-location-name": { "t-out": () => this.el.dataset.locationName },
+        ".weather-cards-container": {
+            "t-att-class": () => ({ "row": !!this.locationName }),
+        },
+    };
+
+    /**
+     * Do in `setup` everything that is already available before the
+     * interaction is started. Only synchronous stuff.
+     */
+    setup() {
+        this.locationName = this.el.dataset.locationName;
+    }
+
+    /**
+     * In `willStart`, keep only asynchronous code without which the interaction
+     * cannot work.
+     */
+    async willStart() {
+        this.weatherData = await rpc("/interactions_demo/get_weather");
+    }
+
+    /**
+     * Do in `start` what has to wait for the interaction to be started.
+     * Typically: wait for the listeners to be attached, wait for asynchronous
+     * code from `willStart`...
+     */
+    start() {
+        this.days = this.weatherData.daily.temperature_2m_max.map((temperature, index) => {
+            let img;
+            const weatherCode = this.weatherData.daily.weathercode[index];
+            if (weatherCode === 0) {
+                img = "sun";
+            } else if (weatherCode < 3) {
+                img = "sun_clouds";
+            } else if (weatherCode <= 20) {
+                img = "clouds";
+            } else {
+                img = "rain";
+            }
+            return {
+                date: formatDate(
+                    parseDate(this.weatherData.daily.time[index]), { format: "EEEE d MMM" }
+                ),
+                temp: temperature,
+                img,
+            };
+        }).splice(0, 5);
+
+        this.renderAt(
+            "interactions_demo.s_weather.cards",
+            { days: this.days },
+            this.el.querySelector(".weather-cards-container")
+        );
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("interactions_demo.weather", Weather);
+
+// registry
+//     .category("public.interactions.edit")
+//     .add("interactions_demo.weather", { Interaction: Weather });

AlessandroLupo/oxp2026-custom-website-block

custom_website_block_demo/controllers/controllers.py
@@ -1,0 +1,38 @@
+from odoo import http
+
+
+class CustomWebsiteBlockDemo(http.Controller):
+    @http.route('/custom_website_block_demo/get_weather', auth='public', type='jsonrpc')
+    def get_weather(self):
+        # Data based on API call to Open-Meteo.com
+        return {
+            "daily": {
+                "time": [
+                    "2025-09-18",
+                    "2025-09-19",
+                    "2025-09-20",
+                    "2025-09-21",
+                    "2025-09-22",
+                    "2025-09-23",
+                    "2025-09-24",
+                ],
+                "temperature_2m_max": [
+                    11.2,
+                    11.3,
+                    11.6,
+                    11.9,
+                    10.0,
+                    11.0,
+                    9.6
+                ],
+                "weathercode": [
+                    0,
+                    80,
+                    18,
+                    3,
+                    61,
+                    3,
+                    61
+                ]
+            }
+        }

custom_website_block_demo/static/src/interactions/weather.js
@@ -1,0 +1,73 @@
+import { formatDate, parseDate } from "@web/core/l10n/dates"
+import { rpc } from "@web/core/network/rpc";
+import { registry } from "@web/core/registry";
+import { Interaction } from "@web/public/interaction";
+
+export class Weather extends Interaction {
+    static selector = ".s_weather";
+
+    dynamicContent = {
+        ".weather-location-name": { "t-out": () => this.el.dataset.locationName },
+        ".weather-cards-container": {
+            "t-att-class": () => ({ "row": !!this.locationName }),
+        },
+    };
+
+    /**
+     * Do in `setup` everything that is already available before the
+     * interaction is started. Only synchronous stuff.
+     */
+    setup() {
+        this.locationName = this.el.dataset.locationName;
+    }
+
+    /**
+     * In `willStart`, keep only asynchronous code without which the interaction
+     * cannot work.
+     */
+    async willStart() {
+        this.weatherData = await rpc("/custom_website_block_demo/get_weather");
+    }
+
+    /**
+     * Do in `start` what has to wait for the interaction to be started.
+     * Typically: wait for the listeners to be attached, wait for asynchronous
+     * code from `willStart`...
+     */
+    start() {
+        this.days = this.weatherData.daily.temperature_2m_max.map((temperature, index) => {
+            let img;
+            const weatherCode = this.weatherData.daily.weathercode[index];
+            if (weatherCode === 0) {
+                img = "sun";
+            } else if (weatherCode < 3) {
+                img = "sun_clouds";
+            } else if (weatherCode <= 20) {
+                img = "clouds";
+            } else {
+                img = "rain";
+            }
+            return {
+                date: formatDate(
+                    parseDate(this.weatherData.daily.time[index]), { format: "EEEE d MMM" }
+                ),
+                temp: temperature,
+                img,
+            };
+        }).splice(0, 5);
+
+        this.renderAt(
+            "custom_website_block_demo.s_weather.cards",
+            { days: this.days },
+            this.el.querySelector(".weather-cards-container")
+        );
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("custom_website_block_demo.weather", Weather);
+
+// registry
+//     .category("public.interactions.edit")
+//     .add("custom_website_block_demo.weather", { Interaction: Weather });