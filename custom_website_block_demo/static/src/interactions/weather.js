robinlej/oxp2025-interactions

Step 4: more interaction

interactions_demo/static/src/interactions/weather.js
@@ -7,10 +7,13 @@ export class Weather extends Interaction {
     static selector = ".s_weather";
 
     dynamicContent = {
-        ".weather-location-name": { "t-out": () => this.el.dataset.locationName },
+        ".weather-location-name": {
+            "t-out": () => this.locationName.charAt(0).toUpperCase() + this.locationName.slice(1),
+        },
         ".weather-cards-container": {
             "t-att-class": () => ({ "row": !!this.locationName }),
         },
+        "#weather-location-input": { "t-on-change": this.onInputChange },
     };
 
     /**
@@ -19,6 +22,7 @@ export class Weather extends Interaction {
      */
     setup() {
         this.locationName = this.el.dataset.locationName;
+        this.cardsContainerEl = this.el.querySelector(".weather-cards-container");
     }
 
     /**
@@ -26,7 +30,10 @@ export class Weather extends Interaction {
      * cannot work.
      */
     async willStart() {
-        this.weatherData = await rpc("/interactions_demo/get_weather");
+        this.weatherData = await rpc(
+            "/interactions_demo/get_weather",
+            { location: this.locationName }
+        );
     }
 
     /**
@@ -35,11 +42,22 @@ export class Weather extends Interaction {
      * code from `willStart`...
      */
     start() {
+        this.updateWeather();
+    }
+
+    updateWeather() {
+        if (this.weatherData.error) {
+            return;
+        }
         this.days = this.weatherData.daily.temperature_2m_max.map((temperature, index) => {
             let img;
             const weatherCode = this.weatherData.daily.weathercode[index];
             if (weatherCode === 0) {
                 img = "sun";
+            } else if (weatherCode === -1) {
+                img = "sun_hot";
+            } else if (weatherCode === -2) {
+                img = "unicorn";
             } else if (weatherCode < 3) {
                 img = "sun_clouds";
             } else if (weatherCode <= 20) {
@@ -56,11 +74,25 @@ export class Weather extends Interaction {
             };
         }).splice(0, 5);
 
+        // `removeChildren` and `renderAt` are Interaction utils. You don't have
+        // to think about undoing them in the `destroy` method: it's handled by
+        // design.
+        // This goes for all the default methods that you can find in
+        // `web/static/src/public/interaction.js`.
+        this.removeChildren(this.cardsContainerEl);
         this.renderAt(
             "interactions_demo.s_weather.cards",
             { days: this.days },
-            this.el.querySelector(".weather-cards-container")
+            this.cardsContainerEl
         );
+    }
+
+    async onInputChange(ev) {
+        this.locationName = ev.currentTarget.value;
+        this.weatherData = await this.waitFor(
+            rpc("/interactions_demo/get_weather", { location: this.locationName })
+        );
+        this.updateWeather();
     }
 }
 
@@ -68,6 +100,6 @@ registry
     .category("public.interactions")
     .add("interactions_demo.weather", Weather);
 
-// registry
-//     .category("public.interactions.edit")
-//     .add("interactions_demo.weather", { Interaction: Weather });
+registry
+    .category("public.interactions.edit")
+    .add("interactions_demo.weather", { Interaction: Weather });

AlessandroLupo/oxp2026-custom-website-block

custom_website_block_demo/static/src/interactions/weather.js
@@ -7,10 +7,13 @@ export class Weather extends Interaction {
     static selector = ".s_weather";
 
     dynamicContent = {
-        ".weather-location-name": { "t-out": () => this.el.dataset.locationName },
+        ".weather-location-name": {
+            "t-out": () => this.locationName?.charAt(0).toUpperCase() + this.locationName?.slice(1),
+        },
         ".weather-cards-container": {
             "t-att-class": () => ({ "row": !!this.locationName }),
         },
+        "#weather-location-input": { "t-on-change": this.onInputChange },
     };
 
     /**
@@ -19,6 +22,7 @@ export class Weather extends Interaction {
      */
     setup() {
         this.locationName = this.el.dataset.locationName;
+        this.cardsContainerEl = this.el.querySelector(".weather-cards-container");
     }
 
     /**
@@ -26,7 +30,10 @@ export class Weather extends Interaction {
      * cannot work.
      */
     async willStart() {
-        this.weatherData = await rpc("/custom_website_block_demo/get_weather");
+        this.weatherData = await rpc(
+            "/custom_website_block_demo/get_weather",
+            { location: this.locationName }
+        );
     }
 
     /**
@@ -35,11 +42,22 @@ export class Weather extends Interaction {
      * code from `willStart`...
      */
     start() {
+        this.updateWeather();
+    }
+
+    updateWeather() {
+        if (this.weatherData.error) {
+            return;
+        }
         this.days = this.weatherData.daily.temperature_2m_max.map((temperature, index) => {
             let img;
             const weatherCode = this.weatherData.daily.weathercode[index];
             if (weatherCode === 0) {
                 img = "sun";
+            } else if (weatherCode === -1) {
+                img = "sun_hot";
+            } else if (weatherCode === -2) {
+                img = "unicorn";
             } else if (weatherCode < 3) {
                 img = "sun_clouds";
             } else if (weatherCode <= 20) {
@@ -56,11 +74,25 @@ export class Weather extends Interaction {
             };
         }).splice(0, 5);
 
+        // `removeChildren` and `renderAt` are Interaction utils. You don't have
+        // to think about undoing them in the `destroy` method: it's handled by
+        // design.
+        // This goes for all the default methods that you can find in
+        // `web/static/src/public/interaction.js`.
+        this.removeChildren(this.cardsContainerEl);
         this.renderAt(
             "custom_website_block_demo.s_weather.cards",
             { days: this.days },
-            this.el.querySelector(".weather-cards-container")
+            this.cardsContainerEl
         );
+    }
+
+    async onInputChange(ev) {
+        this.locationName = ev.currentTarget.value;
+        this.weatherData = await this.waitFor(
+            rpc("/custom_website_block_demo/get_weather", { location: this.locationName })
+        );
+        this.updateWeather();
     }
 }
 
@@ -68,6 +100,6 @@ registry
     .category("public.interactions")
     .add("custom_website_block_demo.weather", Weather);
 
-// registry
-//     .category("public.interactions.edit")
-//     .add("custom_website_block_demo.weather", { Interaction: Weather });
+registry
+    .category("public.interactions.edit")
+    .add("custom_website_block_demo.weather", { Interaction: Weather });