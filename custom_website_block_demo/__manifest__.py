robinlej/oxp2025-interactions

Step 7: interaction in edit mode

interactions_demo/__manifest__.py
@@ -29,6 +29,10 @@
     'assets': {
         'web.assets_frontend': [
             'interactions_demo/static/src/interactions/**/*',
+            ('remove', 'interactions_demo/static/src/interactions/**/*.edit.js'),
+        ],
+        'website.assets_edit_frontend': [
+            'interactions_demo/static/src/**/*.edit.js',
         ],
     },
     'license': 'LGPL-3',

AlessandroLupo/oxp2026-custom-website-block

custom_website_block_demo/__manifest__.py
@@ -29,6 +29,10 @@
     'assets': {
         'web.assets_frontend': [
             'custom_website_block_demo/static/src/interactions/**/*',
+            ('remove', 'custom_website_block_demo/static/src/interactions/**/*.edit.js'),
+        ],
+        'website.assets_inside_builder_iframe': [
+            'custom_website_block_demo/static/src/**/*.edit.js',
         ],
     },
     'license': 'LGPL-3',